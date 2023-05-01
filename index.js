d1ec3305aaf26687716d5a80d9781b0068a1bfe2
Mai <garcia.ton94@gmail.com>
2023-05-01 12:29:44 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+     return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(weekdayActivity = 'go to the office') {
+    return `This Monday, I will ${weekdayActivity}.`;
+}
+
+function wrapAdjective(decoration = '*'){
+    return function (descriptor = 'special') {
+        return `You are ${decoration}${descriptor}${decoration}!`;
+    }
+}
+
